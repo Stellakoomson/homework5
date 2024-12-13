d386a0f3a0cedd50dbb6e47a543561e92ec63d16
Stellakoomson <stellakoomson006@gmail.com>
2024-12-13 11:32:10 +0000

    adding all files

---
+++ src/index.js
@@ -0,0 +1,15 @@
+let y = prompt("Name");
+
+alert(y)
+
+
+let w = prompt("age");
+
+alert(w)
+
+
+let E = prompt('email');
+
+alert(E)
+
+alert(`hi ${y}, you are ${w} years old and we will keep in touch via your email ${E}`);
